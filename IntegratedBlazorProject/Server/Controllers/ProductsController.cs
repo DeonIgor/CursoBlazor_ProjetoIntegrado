3ead76c4a09589a2b268a95fccdfedabc392baea
DeonIgor <igordeon709@gmail.com>
2023-08-21 01:28:48 -0300

    Add ProductController API / app.config / BD connection

---
+++ IntegratedBlazorProject/Server/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using IntegratedBlazorProject.Shared.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace IntegratedBlazorProject.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly string ConnString;
+
+        public ProductsController()
+        {
+            ConnString = System.Configuration.ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            using (var connection = new SqlConnection(ConnString))
+            {
+                var sql = "";
+                // var command = connection.Query<Product>(sql).AsList();
+                // Console.WriteLine(command);
+
+                return null;
+            }
+        }
+    }
+}
