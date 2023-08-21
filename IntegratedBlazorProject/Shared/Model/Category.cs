15965961679e143773786aa1f501525f1412d123
Igor Deon <igordeon709@gmail.com>
2023-08-21 14:56:14 -0300

    Add Category / Change View

---
+++ IntegratedBlazorProject/Shared/Model/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntegratedBlazorProject.Shared.Model
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+
+        [Required(ErrorMessage = "*Campo Obrigatório")]
+        [MaxLength(50, ErrorMessage = "*Máximo de 50 Caracteres")]
+        public string Name { get; set; }
+
+        public Category() 
+        {
+            this.CategoryId = Guid.NewGuid();
+        }
+
+        public Category(string name)
+        {
+            this.CategoryId = Guid.NewGuid();
+            this.Name = name;
+        }
+    }
+}
--- IntegratedBlazorProject/Shared/Model/Product.cs
+++ IntegratedBlazorProject/Shared/Model/Product.cs
@@ -4,29 +4,42 @@ namespace IntegratedBlazorProject.Shared.Model
 {
     public class Product
     {
+        public Guid ProductId { get; set; }
+
         [Required(ErrorMessage = "*Campo Obrigatório")]
         [MaxLength(50, ErrorMessage = "*Máximo de 50 Caracteres")]
         public string? Name { get; set; }
 
         [MaxLength(150, ErrorMessage = "*Máximo de 150 Caracteres")]
         public string? Description { get; set; }
-        public string? ProductId { get; set; }
 
         [Required(ErrorMessage = "*Campo Obrigatório")]
         [Range(0.0, float.MaxValue, ErrorMessage = "*O Preço Deve Ser Um Valor Positivo")]
         public float? Price { get; set; }
 
+        public Category Category { get; set; }
+
         public Product() 
         {
-            ProductId = Guid.NewGuid().ToString();
+            this.ProductId = Guid.NewGuid() ;
+            this.Category = new Category();
         }
 
-        public Product(string name, string description, float price)
+        public Product(string name, string description, float price, Category category)
         {
-            Name = name;
-            Description = description;
-            ProductId = Guid.NewGuid().ToString();
-            Price = price;
+            this.ProductId = Guid.NewGuid();
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+            this.Category = category;
+        }
+
+        public Product(string name, float price, Category category)
+        {
+            this.ProductId = Guid.NewGuid();
+            this.Name = name;
+            this.Price = price;
+            this.Category = category;
         }
     }
 }
